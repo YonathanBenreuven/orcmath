38a4b21200c1795178abbf487c8584c85e1cb68e
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-12-20 11:01:15 -0500

    Life ay

---
+++ OrcMathGUI/src/yonathanPlayground/CatalogMaker1.java
@@ -0,0 +1,61 @@
+package yonathanPlayground;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CatalogMaker1 {
+ private static ArrayList<Cards> cardList;
+ 
+ 	public CatalogMaker1() {
+ 		cardList = new ArrayList<Cards>();
+ 		cardList.add(new Cards("imp","1/1",1));
+ 		cardList.add(new Cards("doubleimp","2/2",2));
+ 		cardList.add(new Cards("tripelimp","3/3",3));
+	}
+
+	public static void main(String[] args) {
+		
+		CatalogMaker1 c = new CatalogMaker1();
+		System.out.println(getCSVContent());
+		//for(Cards e:cardList) {
+			//System.out.println(e);
+		//}
+		
+		while(cardList.size()<10) {	 
+			 addCard();
+			 System.out.println(getCSVContent());
+			
+			}
+		}
+	
+	public static void addCard() {
+		System.out.println("what is the name of the card you want to add");
+		Scanner in = new Scanner(System.in);
+		 String s = in.nextLine();
+		 
+		 System.out.println("what is the effect or stats of the card you want to add");
+			
+			 String d = in.nextLine();
+			 
+			 System.out.println("what is the cost of the card you want to add");
+				
+			 int f = Integer.parseInt(in.nextLine());
+			 
+			 
+			 System.out.println(getCSVContent());
+			cardList.add(new Cards(s,d,f));
+		
+		
+	}
+	public static void save() {
+	//	String[] Data = cardList.split(",");
+	}
+	public static String getCSVContent() {
+		String data = "";
+		for(Cards e:cardList) {
+			data+=e+"\n";
+		}
+		return data;
+	}
+
+}
--- OrcMathGUI/src/yonathanPlayground/CatalogScreen.java
+++ OrcMathGUI/src/yonathanPlayground/CatalogScreen.java
@@ -1,14 +1,23 @@
 package yonathanPlayground;
 
+import java.io.File;
 import java.util.List;
 
-import guiTeacher.components.TextField;
+import javax.swing.JFrame;
+
+import guiTeacher.components.*;
+import guiTeacher.interfaces.FileRequester;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class CatalogScreen extends FullFunctionScreen {
- 
+public class CatalogScreen extends FullFunctionScreen implements FileRequester {
+	private TextArea text;
 	private TextField descriptionField;
+	private Button addButton;
+	private Button saveButton;
+	private Button deleteButton;
+	private FileOpenButton openFile;
+	private CatalogMaker cardMaker;
 	public CatalogScreen(int width, int height) {
 		super(width, height);
 		
@@ -16,17 +25,65 @@ public class CatalogScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		descriptionField = new TextField(40, 40, 200, 30, "enter text","card name");
+		addButton = new Button(300,300,50,50,"add",new Action() {
+			
+			@Override
+			public void act() {
+				addClicked();
+				
+			}
+		});
+		viewObjects.add(addButton);
+		saveButton = new Button(300,360,50,50,"save",new Action() {
+			
+			@Override
+			public void act() {
+				addClicked();
+				
+			}
+		});
+		viewObjects.add(saveButton);
+		deleteButton = new Button(300,420,50,50,"delete",new Action() {
+			
+			@Override
+			public void act() {
+				addClicked();
+				
+			}
+		});
+		viewObjects.add(deleteButton);
+		openFile = new FileOpenButton(300,480,50,50,null,this);
+		viewObjects.add(openFile);
+		text = new TextArea(80,80,100,100,"This is where text goes");
+		viewObjects.add(text);
+		descriptionField = new TextField(40, 40, 100, 40, "enter text","card name");
 		viewObjects.add(descriptionField);
-		descriptionField = new TextField(40, 40, 200, 30, "enter text","card effect");
+		descriptionField = new TextField(140, 140, 200, 40, "enter text","card effect");
 		viewObjects.add(descriptionField);
-		descriptionField = new TextField(40, 40, 200, 30, "enter text","card cost");
+		descriptionField = new TextField(240, 240, 200, 40, "enter text","card cost");
 		viewObjects.add(descriptionField);
+	}
+
+	protected void addClicked() {
+		text.setText("add button clicked");
+		
 	}
 
 	public static void main(String[] args) {
 	
 
+	}
+
+	@Override
+	public void setFile(File f) {
+		
+		
+	}
+
+	@Override
+	public JFrame getWindow() {
+		
+		return null;
 	}
 
 }
