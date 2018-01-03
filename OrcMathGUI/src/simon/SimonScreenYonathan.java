169b310f702af045b7563b150d74fe036fbf54cc
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-03 11:40:49 -0500

    fields

--- OrcMathGUI/src/simon/SimonScreenYonathan.java
+++ OrcMathGUI/src/simon/SimonScreenYonathan.java
@@ -1,11 +1,28 @@
 package simon;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
 
+
 public class SimonScreenYonathan extends ClickableScreen implements Runnable {
+
+	protected static Object card;
+	protected static Object inside;
+	private TextLabel textLabel;
+	private ButtonInterfaceYonathan[] buttonInterface;
+	private ProgressInterfaceYonathan progressInterface;
+	private ArrayList<MoveInterfaceYonathan> moveInterface;
+	int roundNumber;
+	boolean acceptingInput;
+	int sequenceIndex;
+	int lastSelectedButton;
 
 	public SimonScreenYonathan(int width, int height) {
 		super(width, height);
@@ -25,7 +42,10 @@ public class SimonScreenYonathan extends ClickableScreen implements Runnable {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/winterscape.jpg"));
+		
+		
+		//viewObjects.add();
 		
 	}
 
